33413f0fac4e7e81d4a2e5b99a66c3e2495267e7
l3aichaDev <mr.izedtea@gmail.com>
2015-12-09 17:27:52 +0700

    first commit

--- core/src/com/packtpub/libgdx/game/WorldController.java
+++ core/src/com/packtpub/libgdx/game/WorldController.java
@@ -1,5 +1,9 @@
 package com.packtpub.libgdx.game;
 
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
 import com.packtpub.libgdx.canyonbunny.CanyonBunnyMain;
 
 /**
@@ -9,9 +13,57 @@ public class WorldController {
 
     public static final String TAG = CanyonBunnyMain.class.getName();
 
-    public WorldController() { }
+    public Sprite[] testSprites;
+    public int selectedSprite;
 
-    public void init() { }
+    public WorldController() {
+        init();
+    }
 
-    public void update(float deltaTime) { }
+    public void init() {
+        initTestObjects();
+    }
+
+    private void initTestObjects() {
+        testSprites = new Sprite[5];
+        int width = 32;
+        int height = 32;
+        Pixmap pixmap = createProceduralPixmap(width, height);
+        Texture texture = new Texture(pixmap);
+
+        for (int i = 0; i < testSprites.length; i++) {
+            Sprite spr = new Sprite(texture);
+            spr.setSize(1, 1);
+            spr.setOrigin(spr.getWidth() / 2.0f, spr.getHeight() / 2.0f);
+            float randomX = MathUtils.random(-2.0f, 2.0f);
+            float randomY = MathUtils.random(-2.0f, 2.0f);
+            spr.setPosition(randomX, randomY);
+            testSprites[i] = spr;
+        }
+
+        selectedSprite = 0;
+    }
+
+    private Pixmap createProceduralPixmap(int width, int height) {
+        Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
+        pixmap.setColor(1, 0, 0, 0.5f);
+        pixmap.fill();
+        pixmap.setColor(1, 1, 0, 1);
+        pixmap.drawLine(0, 0, width, height);
+        pixmap.drawLine(width, 0, 0, height);
+        pixmap.setColor(0, 1, 1, 1);
+        pixmap.drawRectangle(0, 0, width, height);
+        return pixmap;
+    }
+
+    public void update(float deltaTime) {
+        updateTestObjects(deltaTime);
+    }
+
+    private void updateTestObjects(float deltaTime) {
+        float rotation = testSprites[selectedSprite].getRotation();
+        rotation += 90 * deltaTime;
+        rotation %= 360;
+        testSprites[selectedSprite].setRotation(rotation);
+    }
 }
--- core/src/com/packtpub/libgdx/game/WorldRenderer.java
+++ core/src/com/packtpub/libgdx/game/WorldRenderer.java
@@ -1,8 +1,10 @@
 package com.packtpub.libgdx.game;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
+import com.packtpub.libgdx.util.Constants;
 
 /**
  * Created by anuchapa on 9/12/2015.
@@ -13,16 +15,38 @@ public class WorldRenderer implements Disposable {
     private SpriteBatch batch;
     private WorldController worldController;
 
-    public WorldRenderer(WorldController worldController) { }
+    public WorldRenderer(WorldController worldController) {
+        this.worldController = worldController;
+        init();
+    }
 
-    private void init() { }
+    private void init() {
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
+        camera.position.set(0, 0, 0);
+        camera.update();
+    }
 
-    public void render() { }
+    public void render() {
+        renderTestObjects();
+    }
 
-    public void resize(int width, int height) { }
+    private void renderTestObjects() {
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        for(Sprite sprite : worldController.testSprites) {
+            sprite.draw(batch);
+        }
+        batch.end();
+    }
+
+    public void resize(int width, int height) {
+        camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / height) * width;
+        camera.update();
+    }
 
     @Override
     public void dispose() {
-
+        batch.dispose();
     }
 }
